0d4ff377b5d65ca49ddc6b28a4cc700c08c528b2
hya <hyasysujx@163.com>
2026-02-05 12:25:30 +0800

    update setup.sh to download nvim config

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# download nvim config
+git submodule init
+git submodule update
+
 # create symlinks of my dotfiles (will not override if already exists)
 [ -d "$HOME/.config" ] && ln -s $HOME/dotfiles/.config/* $HOME/.config || ln -s $HOME/dotfiles/.config $HOME
