1901eea336955056dc9ea82a7d71686ecae011e3
hya <hyasysujx@163.com>
2026-02-06 17:29:10 +0800

    specify branch

--- setup.sh
+++ setup.sh
@@ -3,6 +3,11 @@
 # download nvim config
 git submodule init
 git submodule update --recursive --remote
+git submodule foreach --recursive '
+    git checkout $(git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@") ||
+    git checkout main ||
+    git checkout master
+'
 
 # create symlinks of my dotfiles (will not override if already exists)
 [ -d "$HOME/.config" ] && ln -s $HOME/dotfiles/.config/* $HOME/.config || ln -s $HOME/dotfiles/.config $HOME
